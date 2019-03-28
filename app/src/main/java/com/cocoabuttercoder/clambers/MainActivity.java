59921e8e2853dd46f95fae5906850f6c7d812a31
Bryan <cocoabuttercoder@gmail.com>
2019-03-28 18:05:34 -0400

    Tried to AlphabetSoup into background

--- app/src/main/java/com/cocoabuttercoder/clambers/MainActivity.java
+++ app/src/main/java/com/cocoabuttercoder/clambers/MainActivity.java
@@ -1,9 +1,13 @@
 package com.cocoabuttercoder.clambers;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -18,6 +22,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        RelativeLayout relativeLayout = findViewById(R.id.relativeLayout);
+
+        for(int i = 0; i < 25; i++){
+            Random letterRNG = new Random();
+            Random rotationRNG = new Random();
+            Random xRNG = new Random();
+            Random yRNG = new Random();
+            int letterAsInt = letterRNG.nextInt(26);
+            float rotation = rotationRNG.nextFloat() * 360;
+            float xCoordinate = xRNG.nextFloat() * relativeLayout.getWidth();
+            float yCoordinate = yRNG.nextFloat() * relativeLayout.getHeight();
+            String letter = String.valueOf((char)(65 + letterAsInt));
+            TextView character = new TextView(this);
+
+            character.setText(letter);
+            character.setRotation(rotation);
+            character.setX(xCoordinate);
+            character.setTextColor(Color.rgb(255, 193, 7));
+            character.setY(yCoordinate);
+            character.setTextSize(TypedValue.COMPLEX_UNIT_SP, 34);
+
+            relativeLayout.addView(character);
+        }
     }
 
     public void scramble(View view){
